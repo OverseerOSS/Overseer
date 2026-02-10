a62147f8937336fa18e2949d0dc648327cf5bbd5
Elias <lindholmelias50+123@gmail.com>
2026-02-10 12:03:20 +0000

    Import and register Dokploy and Linux Server plugins from OverseerOSS/plugins

--- app/extensions/loader.ts
+++ app/extensions/loader.ts
@@ -1,18 +1,16 @@
 import "server-only";
 import { MonitoringExtension } from "./types";
 
-// Static import of extensions would go here
-// import * as Dokploy from "./dokploy"; 
-// import * as LinuxServer from "./linux-server";
+// Static import of extensions
+import { dokployExtension } from "./dokploy"; 
+import { linuxServerExtension } from "./linux-server";
 
 export async function loadExtensions(): Promise<MonitoringExtension[]> {
   const extensions: MonitoringExtension[] = [];
 
   // Manual registration to avoid dynamic import issues in Next.js
-  /*
-  if (isValidExtension(Dokploy.default)) extensions.push(Dokploy.default);
-  if (isValidExtension(LinuxServer.default)) extensions.push(LinuxServer.default);
-  */
+  if (isValidExtension(dokployExtension)) extensions.push(dokployExtension);
+  if (isValidExtension(linuxServerExtension)) extensions.push(linuxServerExtension);
   
   return extensions;
 }
